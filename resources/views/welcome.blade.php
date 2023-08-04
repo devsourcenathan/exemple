9d41074c492e595ac2a87bd69ec74767c4205cfb
Nathan dev <sadenachbinathan@gmail.com>
2023-08-04 12:48:17 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Nathan is here !</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Salut c'est Nathan.
+                    Je teste moi seulement les choses hein.
                 </div>
 
                 <div class="links">
